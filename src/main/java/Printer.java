dca48bcb109e8c13a92362a5f063bccda0ce19b2
Jasmine Mills <jazzrmills@gmail.com>
2022-06-13 17:22:21 +0100

    extension completed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,32 @@
 public class Printer {
+
+    private int sheetsOfPaper;
+    private int tonerVolume;
+
+    public Printer(int sheetsOfPaper, int tonerVolume){
+        this.sheetsOfPaper = sheetsOfPaper;
+        this.tonerVolume = tonerVolume;
+
+    }
+
+    public int getSheetsOfPaper() {
+        return sheetsOfPaper;
+    }
+
+
+    public void print(int pages, int copies) {
+        int total = pages * copies;
+        int tonerUsed = total;
+        if (total <= sheetsOfPaper){
+            sheetsOfPaper -= total;
+            tonerVolume -= tonerUsed;
+
+        }
+
+
+    }
+
+    public int getTonerVolume() {
+        return tonerVolume;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,3 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
-    
+
+    Printer myPrinter;
+
+    @Before
+    public void before(){
+        myPrinter = new Printer(50, 300);
+    }
+
+    @Test
+    public void countSheetsOfPaper(){
+        assertEquals(50,myPrinter.getSheetsOfPaper());
+    }
+
+    @Test
+    public void tryToPrint10Pages(){
+        myPrinter.print(10, 1);
+        assertEquals(40,myPrinter.getSheetsOfPaper());
+    }
+
+    @Test
+    public void tryToPrint30Pages(){
+        myPrinter.print(10, 1);
+        myPrinter.print(20,1);
+        assertEquals(20,myPrinter.getSheetsOfPaper());
+    }
+
+    @Test
+    public void tryToPrint2Copies(){
+        myPrinter.print(20, 2);
+        assertEquals(10,myPrinter.getSheetsOfPaper());
+    }
+
+    @Test
+    public void tryToPrintTooManyCopies(){
+        myPrinter.print(20, 3);
+        assertEquals(50,myPrinter.getSheetsOfPaper());
+
+    }
+    @Test
+    public void checkTonerVolumeReduced(){
+        myPrinter.print(20, 2);
+        assertEquals(260,myPrinter.getTonerVolume());
+    }
+
+
 }
